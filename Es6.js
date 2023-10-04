f3a876f3933c38e908446615d9335dad42f10dfe
Olha Kutniv <olhakutniv@symphony-solutions.com>
2023-10-05 00:04:18 +0200

    Homework JS

---
+++ Es6.js
@@ -0,0 +1,35 @@
+class Computer {
+  constructor (manufacturer, model) {
+    this.manufacturer = manufacturer;
+    this.model = model;
+    this.isPoweredOn = false;
+  }
+  PowerOn() {
+    if (!this.isPoweredOn) {
+      this.isPoweredOn = true;
+      // Please pay attention on operation adding implemented on string!
+      // Investigate the topic - Operations in JavaScript!
+  
+      console.log(`${this.manufacturer} and ${this.model} is now powered on.`);
+    } else {
+      console.log(
+        `${this.manufacturer} and ${this.model} is already powered on.`
+      );
+    }
+  }
+  PowerOff() {
+    if (this.isPoweredOn) {
+      this.isPoweredOn = false;
+      console.log(`${this.manufacturer} and ${this.model} is now powered 
+off.`);
+    } else {
+      console.log(
+        `${this.manufacturer} and ${this.model} is already powered off.`
+      );
+    }
+  }
+
+
+}
+const computer1 = new Computer("Apple,", "MacBookPro")
+const computer2 = new Computer("HP", "HPProBoook")
